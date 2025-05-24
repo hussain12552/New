666a4e40cc0417020300fc2d21bf79cb5a3224ca
Hussain <hussain.ismail12552@gmail.com>
2025-05-24 23:59:12 +0530

    add files

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+def greet(name):
+    """Function to greet a person with their name."""
+    return f"Hello, {name}!"
+input_name = input("Enter your name: ")
+print(greet(input_name))
