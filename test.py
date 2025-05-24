8db5aec1cbe878a779709710b4c4944b1d747f6b
Hussain <hussain.ismail12552@gmail.com>
2025-05-25 00:03:16 +0530

    Add comments and a print

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ def greet(name):
     return f"Hello, {name}!"
 input_name = input("Enter your name: ")
 print(greet(input_name))
+# This is a simple Python script that greets the user.
+# It defines a function `greet` that takes a name as an argument and returns a greeting message.
+print("This script is a demonstration of a simple Python function.")
+# The script also includes a user input prompt to get the name from the user.
